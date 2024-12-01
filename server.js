a68cb9f12eafc64e5d24c2fb1ebf2d4570f11bf1
ZzJoaoGustazZ <joaogustavounifor@gmail.com>
2024-12-01 18:26:33 -0300

    mudei a porta

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3300;
+const port = 53494;
 
 // Middleware para analisar o corpo da requisição como JSON
 app.use(express.json());
